b0970fb311e49e01003f6cfc33d0f84e83c68e51
winithraKS <winithra.khaosauy@gmail.com>
2024-11-19 21:55:07 +0700

    commit 16

--- src/libs/getCampground.ts
+++ src/libs/getCampground.ts
@@ -1,5 +1,9 @@
 export default async function getCampground(id:string) {
-    const response = await fetch(`https://campground-backend-kappa.vercel.app:443/api/v1/campgrounds/${id}`,{next:{tags:['campground']}})
+    const response = await fetch(`https://campground-backend-kappa.vercel.app:443/api/v1/campgrounds/${id}`,
+        {next:{tags:['campground']},
+        cache: 'no-cache'}
+        
+    )
     
     if(!response.ok) throw new Error("fail to get campground")
     return await response.json()
--- src/libs/getCampgrounds.ts
+++ src/libs/getCampgrounds.ts
@@ -1,9 +1,7 @@
 export default async function getCampgrounds() {
     await new Promise((resolve)=>setTimeout(resolve,1500))
 
-    const response = await fetch("https://campground-backend-kappa.vercel.app:443/api/v1/campgrounds")
+    const response = await fetch("https://campground-backend-kappa.vercel.app:443/api/v1/campgrounds",{cache: 'no-cache'})
     if(!response.ok) throw new Error("failed to get campground list")
-    const data = await response.json()
-    console.log(data)
-    return data
+    return await response.json()
 }
